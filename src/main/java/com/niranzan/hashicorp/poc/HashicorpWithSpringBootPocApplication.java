f77d2011eb1585ec3860d8242ccba6f82e14c8cb
Niranjan Maharana <niranjan.maharana@maersk.com>
2023-07-20 11:51:30 +0530

    Initial commit

---
+++ src/main/java/com/niranzan/hashicorp/poc/HashicorpWithSpringBootPocApplication.java
@@ -0,0 +1,13 @@
+package com.niranzan.hashicorp.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashicorpWithSpringBootPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashicorpWithSpringBootPocApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/niranzan/hashicorp/poc/rest/MyController.java
@@ -0,0 +1,27 @@
+package com.niranzan.hashicorp.poc.rest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@Value("${knf.secret1}")
+	String secret1;
+
+	@Value("${knf.secret2}")
+	String secret2;
+
+	@GetMapping("/secret")
+	public Map<String, String> getSecret() {
+		Map<String, String> map = new HashMap<>();
+		map.put("Secret 1", secret1);
+		map.put("Secret 2", secret2);
+
+		return map;
+	}
+}
